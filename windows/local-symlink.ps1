b1c3adc2a531ac8bab9ce02373f30810f393cc2a
Chris Chou <chhschou@gmail.com>
2016-12-08 22:56:01 +1300

    Add powershell user profile script and local machine symlink (for development)

---
+++ windows/local-symlink.ps1
@@ -0,0 +1,9 @@
+# This script is totally tailored to my machine atm, will refactor in the future
+
+
+#opengl - GLFW
+cmd /c mklink /D c:\Users\cchou\dev\deps\opengl\include\GLFW c:\Users\cchou\dev\deps\opengl\glfw-3.2.1\include\GLFW
+cmd /c mklink /D c:\Users\cchou\dev\deps\opengl\lib\GLFW C:\Users\cchou\dev\deps\opengl\glfw-3.2.1\build\src\Debug
+#opengl - GLEW
+cmd /c mklink /D c:\Users\cchou\dev\deps\opengl\include\GL c:\Users\cchou\dev\deps\opengl\glew-2.0.0\include\GL
+cmd /c mklink /D c:\Users\cchou\dev\deps\opengl\lib\GL C:\Users\cchou\dev\deps\opengl\glew-2.0.0\build\vc14\lib\Debug

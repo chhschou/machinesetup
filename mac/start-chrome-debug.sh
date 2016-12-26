b4da712f36f8fedde50dae998a583ca61786f07d
Chris Chou <changhsinchou@gmail.com>
2016-12-26 20:24:11 +1300

    add checking if chrome canary is installed, if so use that instead of plain chrome for debug

--- mac/start-chrome-debug.sh
+++ mac/start-chrome-debug.sh
@@ -1,4 +1,21 @@
 #!/usr/bin/env bash
 
 ## start chrome with remote debugger on port 9222
-/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --remote-debugging-port=9222
+set -ex
+
+GCHROME='/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+GCHROMECANARY='/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary'
+
+REMOTEDEBUGGINGPORT="9222"
+REMOTEDEBUGGINGPORTARG="--remote-debugging-port=$REMOTEDEBUGGINGPORT"
+
+if [ -e "$GCHROMECANARY" ] # note: need space between arg and square bracket
+then
+    CMD="$GCHROMECANARY"
+elif [ -e "$GCHROME" ]
+then
+    CMD="$GCHROME"
+fi
+
+# run
+"$CMD" "$REMOTEDEBUGGINGPORTARG"

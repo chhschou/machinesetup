90feb9e3b5d7ec35e1e519980b49c556447ff636
Chris Chou <chhschou@gmail.com>
2017-09-12 22:34:58 +1200

    cleanup

--- vim/mac/pathogen/install.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-## install to user dir
-mkdir -p ~/.vim/autoload ~/.vim/bundle
-
-## download pathogen.vim to dir
-curl -Sso ~/.vim/autoload/pathogen.vim \
-    https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
--- vim/mac/solarized/install.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-## install solarized using pathogen (make sure git is installed first)
-../pathogen/install.sh # install pathogen
-# clone into place
-git clone git://github.com/altercation/vim-colors-solarized.git ~/.vim/bundle/vim-colors-solarized
